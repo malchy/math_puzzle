9c67e5fa8ec60ad8f11c84b27213e7249621e81c
Mal Tok <yusatoku@gmail.com>
2023-04-23 22:57:48 +0900

    Q04まで

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,8 @@ mod questions;
 fn main() {
     //questions::q01::q01();
     //questions::q02::q02();
-    questions::q03::q03_2();
+    //questions::q03::q03_1();
+    //questions::q03::q03_2();
+    //questions::q04::q04_1();
+    questions::q04::q04_2();
 }
